c42a33ca7ad09baae5a9c851ffa46b1c41531c8e
maurocosentino <mauroxcosentino@gmail.com>
2026-05-03 17:56:18 -0300

    feat: add main entry point with basic argument handling

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,21 @@
 #include <iostream>
 
-// TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+int main(int argc, char* argv[]) {
 
-int main() {
-    // TIP Press <shortcut actionId="RenameElement"/> when your caret is at the <b>lang</b> variable name to see how CLion can help you rename it.
+    if (argc < 2) {
+        std::cout << "Uso: soundgen <comando> [opciones]\n";
+        std::cout << "Ejemplo: soundgen generate --wave sine --freq 440\n";
+        return 1;
+    }
 
-    const auto lang = "C++";
-    std::cout << "Hello and welcome to " << lang << "!\n";
+    const std::string subcommand = argv[1];
 
-    for (int i = 1; i <= 5; i++) {
-        // TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        std::cout << "i = " << i << std::endl;
+    if (subcommand == "generate") {
+        std::cout << "Comando: generate (aún no implementado)\n";
+    } else {
+        std::cerr << "Error: comando desconocido '" << subcommand << "'\n";
+        return 1;
     }
 
     return 0;
-    // TIP See CLion help at <a href="https://www.jetbrains.com/help/clion/">jetbrains.com/help/clion/</a>. Also, you can try interactive lessons for CLion by selecting 'Help | Learn IDE Features' from the main menu.
 }
